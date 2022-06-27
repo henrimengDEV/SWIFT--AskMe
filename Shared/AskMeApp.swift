b28b4d8140c42afa1ad07fc4572b45818207df1f
mickael aknine <akninemickael@gmail.com>
2022-06-27 18:05:12 +0200

    Initial Commit

---
+++ Shared/AskMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskMeApp.swift
+//  Shared
+//
+//  Created by Mickael Aknine on 27/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AskMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
